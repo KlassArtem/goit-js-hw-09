f6cc413e5f0a4de3e90c075b1b0b46480e1c61b1
KlassArtem <111056786+KlassArtem@users.noreply.github.com>
2023-01-19 11:54:03 +0200

    added 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,73 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+
+const formCurrentValue = 'form-storage';
+
+let formData = {};
+
+formRef.addEventListener('submit', handleSubmitForm);
+formRef.addEventListener('change', handleInputValue);
+
+getDataFromStorage();
+
+function handleSubmitForm(e) {
+  e.preventDefault();
+
+  startCreatePromises(formData);
+
+  localStorage.removeItem(formCurrentValue);
+  e.target.reset();
+
+  formData = {};
+}
+
+function handleInputValue(e) {
+  formData[e.target.name] = e.target.value.trim();
+
+  const stringifyFormData = JSON.stringify(formData);
+  localStorage.setItem(formCurrentValue, stringifyFormData);
+}
+
+function getDataFromStorage() {
+  const savedMessage = localStorage.getItem(formCurrentValue);
+
+  if (savedMessage) {
+    try {
+      formData = JSON.parse(savedMessage);
+
+      Object.entries(formData).forEach(
+        ([name, value]) => (formRef.elements[name].value = value)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
   }
+}
+
+function startCreatePromises({ delay, step, amount } = {}) {
+  let stepTime = Number(delay);
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, stepTime)
+      .then(({ position, delay }) =>
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      )
+      .catch(({ position, delay }) =>
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
+    stepTime += Number(step);
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
