841cc43f0a03740e8914410087dd7f57e40afd42
KlassArtem <111056786+KlassArtem@users.noreply.github.com>
2023-01-19 11:11:50 +0200

    added 1rst task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
 
+const    startBtn = document.querySelector('button[data-start]');
+const    stopBtn = document.querySelector('button[data-stop]');
+  
+  
+  const timeDelay = 1000;
+  let timerId = null;
+  stopBtn.disabled = true;
+  
+  startBtn.addEventListener('click', handleStartBtnClick);
+  stopBtn.addEventListener('click', handleStopBtnClick);
+  
+  function handleStartBtnClick(event) {
+    timeDelayChangeBgColor();
+    disabledBtnStatusOnStartBtnClick(event);
+  }
+  
+  function handleStopBtnClick(event) {
+    clearInterval(timerId);
+    disabledBtnStatusOnStopBtnClick(event);
+  }
+  
+  function disabledBtnStatusOnStartBtnClick(event) {
+    event.target.disabled = true;
+    stopBtn.disabled = false;
+  }
+  function disabledBtnStatusOnStopBtnClick(event) {
+    event.target.disabled = true;
+    startBtn.disabled = false;
+  }
+  
+  function timeDelayChangeBgColor() {
+    timerId = setInterval(changeBgColor, timeDelay);
+  }
+  
+  function changeBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
